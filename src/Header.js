f1717320f395ab44405678303a272f26a7e2f452
unknown <soleil-2005@mail.ru>
2021-07-22 23:45:38 +0300

    New commit

--- src/Header.js
+++ src/Header.js
@@ -3,14 +3,19 @@ import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
 import tinder_logo from './img/t_logo.png';
+import IconButton from '@material-ui/core/IconButton';
 
 
 const Header = () => {
     return (
         <div className='header'>
-            <PersonIcon fontSize="large"/>
+           <IconButton>
+            <PersonIcon className="header__icon" fontSize="large"/>
+           </IconButton> 
             <img className="header__logo" src={tinder_logo} alt="tinder logo fire"/>
-            <ForumIcon fontSize="large"/>
+           <IconButton> 
+            <ForumIcon className="header__icon" fontSize="large"/>
+           </IconButton> 
         </div>
     )
 }
