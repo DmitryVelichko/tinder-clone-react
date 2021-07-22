9462e42fb5f4f460c86c400e01d45093c26e125a
unknown <soleil-2005@mail.ru>
2021-07-22 17:07:59 +0300

    New commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
        <h1>Let's build TINDER</h1>
+
+       {/* Header */}
+       {/* Tinder Cards */}
+       {/* Buttons below tinder cards */}
+
+       {/* Chats screen */}
+       {/* Individual Chat screen */}
     </div>
   );
 }
