61d52a2954fb5743eebf219485aab0a72ca8070e
unknown <soleil-2005@mail.ru>
2021-07-24 16:25:44 +0300

    New commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+    <Router>  
        {/* Header */}
        <Header />
        {/* Tinder Cards */}
@@ -12,8 +14,21 @@ function App() {
 
        {/* Chats screen */}
        {/* Individual Chat screen */}
+    </Router>   
     </div>
   );
 }
 
 export default App;
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+/*const firebaseConfig = {
+  apiKey: "AIzaSyDF4F-MLxCy_JdTY4ltYrrXAwT4f4LGJYk",
+  authDomain: "tinder-clone-c5cea.firebaseapp.com",
+  projectId: "tinder-clone-c5cea",
+  storageBucket: "tinder-clone-c5cea.appspot.com",
+  messagingSenderId: "466714597857",
+  appId: "1:466714597857:web:3923d445c27026788a5aa0",
+  measurementId: "G-LY3ZM68ZBV"
+};
+*/
