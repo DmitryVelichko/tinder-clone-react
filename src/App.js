28897dd3d33da08c9be0378467fbb3f171f886ed
unknown <soleil-2005@mail.ru>
2021-07-25 11:50:21 +0300

    New commit

--- src/App.js
+++ src/App.js
@@ -25,14 +25,3 @@ function App() {
 
 export default App;
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDF4F-MLxCy_JdTY4ltYrrXAwT4f4LGJYk",
-  authDomain: "tinder-clone-c5cea.firebaseapp.com",
-  projectId: "tinder-clone-c5cea",
-  storageBucket: "tinder-clone-c5cea.appspot.com",
-  messagingSenderId: "466714597857",
-  appId: "1:466714597857:web:3923d445c27026788a5aa0",
-  measurementId: "G-LY3ZM68ZBV"
-};
-*/
