85eb3f605487178dbddddba4b9d36ef4b909ed4e
unknown <soleil-2005@mail.ru>
2021-07-26 16:51:11 +0300

    New commit

--- src/Chat.js
+++ src/Chat.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Avatar from '@material-ui/core/Avatar'
+import './Chat.css'
 
 const Chat = ({ name, message, profilePic, timestamp }) => {
     return (
         <div className="chat">
             <Avatar className="chat__image" alt={name} src={profilePic} />
-            <div>
+            <div className="chat__details">
                 <h2>{name}</h2>
                 <p>{message}</p>
             </div>
+            <p className="chat__timestamp">{timestamp}</p>
         </div>
     )
 }
