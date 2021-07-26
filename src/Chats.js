4b567b25a66dacaf4cdba302dc41f7570360ca8d
unknown <soleil-2005@mail.ru>
2021-07-26 16:41:05 +0300

    New commit

--- src/Chats.js
+++ src/Chats.js
@@ -8,25 +8,25 @@ function Chats() {
                 name="Rachel"
                 message="What's up?"
                 timestamp="40 seconds ago"
-                profilePic="https://unsplash.com/photos/LyeduBb2Auk" 
+                profilePic="https://i.picsum.photos/id/823/5616/3744.jpg?hmac=aUM263SMqOF0mo3zsTkx2otCCV-0D3gyrzULgBEpk5Q" 
             />  
             <Chat 
                 name="Eva"
                 message="Hey, how are you?"
                 timestamp="25 minutes ago"
-                profilePic="https://unsplash.com/photos/U5rMrSI7Pn4" 
+                profilePic="https://i.picsum.photos/id/836/5184/3456.jpg?hmac=_ZfBJnWLbaYFoRWfoj1LNoA_PPzz5EunZNOyXUsUVZ8" 
             />  
             <Chat 
                 name="Zoe"
                 message="Nice photo!"
                 timestamp="2 days ago"
-                profilePic="https://unsplash.com/photos/8wTPqxlnKM4" 
+                profilePic="https://i.picsum.photos/id/832/5472/3648.jpg?hmac=LAAPt-jIUpMimD_tIgqYfATCbzDKKlqZUh6f5dyb5qY" 
             />  
             <Chat 
                 name="Helen"
                 message="Let's talk?"
                 timestamp="1 week ago"
-                profilePic="https://unsplash.com/photos/sseiVD2XsOk" 
+                profilePic="https://i.picsum.photos/id/821/5472/3648.jpg?hmac=QshI0kT8ITshXjqnyV3NOsrvTrH74OHZzEzLM1HOOhk" 
             />  
         </div>
 }
