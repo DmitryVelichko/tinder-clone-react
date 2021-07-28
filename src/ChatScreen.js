4f8a682a7fddeab4e3fec96419011151b7a9cf94
unknown <soleil-2005@mail.ru>
2021-07-28 17:27:22 +0300

    New commit

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './ChatScreen.css'
+import Avatar from '@material-ui/core/Avatar'
 
 function ChatScreen() {
 
@@ -11,7 +12,7 @@ function ChatScreen() {
         },
         {
             name: 'Rachel',
-            image: 'https://i.picsum.photos/id/836/5184/3456.jpg?hmac=_ZfBJnWLbaYFoRWfoj1LNoA_PPzz5EunZNOyXUsUVZ8',
+            image: 'https://i.picsum.photos/id/823/5616/3744.jpg?hmac=aUM263SMqOF0mo3zsTkx2otCCV-0D3gyrzULgBEpk5Q',
             message: 'How is it going?'
         },
         {
@@ -24,6 +25,11 @@ function ChatScreen() {
             <p>YOU MATCHED WITH ELLEN ON 10/08/21</p>
             {messages.map((message) => (
                 <div className="chatScreen__message">
+                    <Avatar 
+                        className="chatScreen__image"
+                        alt={message.name}
+                        src={message.image}
+                    />
                     <p>{message.message}</p>
                 </div>
             ))}
