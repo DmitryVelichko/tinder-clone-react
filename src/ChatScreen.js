6e0ddc4f174e58eeca7b147b8621342305300e6e
unknown <soleil-2005@mail.ru>
2021-07-28 17:23:58 +0300

    New commit

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './ChatScreen.css'
 
 function ChatScreen() {
 
     const [messages, setMessages] = useState([
         {
-            name: 'Ellen',
-            image: '...',
+            name: 'Rachel',
+            image: 'https://i.picsum.photos/id/823/5616/3744.jpg?hmac=aUM263SMqOF0mo3zsTkx2otCCV-0D3gyrzULgBEpk5Q',
             message: 'Whats up'
         },
         {
-            name: 'Ellen',
-            image: '...',
+            name: 'Rachel',
+            image: 'https://i.picsum.photos/id/836/5184/3456.jpg?hmac=_ZfBJnWLbaYFoRWfoj1LNoA_PPzz5EunZNOyXUsUVZ8',
             message: 'How is it going?'
         },
         {
@@ -20,7 +21,12 @@ function ChatScreen() {
 
     return (
         <div className="chatScreen">
-            <h3>Chat Screen</h3>
+            <p>YOU MATCHED WITH ELLEN ON 10/08/21</p>
+            {messages.map((message) => (
+                <div className="chatScreen__message">
+                    <p>{message.message}</p>
+                </div>
+            ))}
         </div>
     )
 }
