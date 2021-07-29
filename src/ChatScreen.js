a551d33370a5015bf8de7cb7f828af3152e08e50
unknown <soleil-2005@mail.ru>
2021-07-29 17:14:35 +0300

    New commit

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -30,7 +30,7 @@ function ChatScreen() {
                         alt={message.name}
                         src={message.image}
                     />
-                    <p>{message.message}</p>
+                    <p className="chatScreen__text">{message.message}</p>
                 </div>
             ))}
         </div>
